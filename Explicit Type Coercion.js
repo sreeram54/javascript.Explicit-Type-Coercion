dfb8bf6d01e1f95975630c2e0484e6f207a2a8f5
sreeram54 <vaddisreeram54@gmail.com>
2022-08-17 09:50:38 +0530

    Explicit Type Coercion

---
+++ Explicit Type Coercion.js
@@ -0,0 +1,32 @@
+var val_1 = "10";
+var val_2 = 10;
+var total = val_1 - val_2;
+
+console.log(total);
+
+var myVar_1 = 10 + ''; 
+console.log(myVar_1); // returned 10 as a String
+
+var myVar_2 = 10.5 + '';
+console.log(myVar_2); // returned 10.5 as a String
+
+var myVar_3 = -10.5 + '';
+console.log(myVar_3); // returned -10.5 as a String
+
+var myVar_4 = true + '';
+console.log(myVar_4); // returned true as a String
+
+
+var val_1 = (10 == '10');
+console.log(val_1); // returns true.
+
+var val_2 = (10 == '5');
+console.log(val_2); // returns false.
+
+var val_3 = (1 == true);
+console.log(val_3); // returns true.
+
+var val_4 = (0 == false);
+console.log(val_4); // returns true.
+
+
